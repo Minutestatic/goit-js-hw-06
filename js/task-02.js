da30ce805d7781c12836169a09c6f17516247a65
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-03-27 04:12:07 +0300

    Виконання другого завдання

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsItems = ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add("iteam");
+
+  return ingredientItem;
+});
+
+ingredientsList.prepend(...ingredientsItems);
