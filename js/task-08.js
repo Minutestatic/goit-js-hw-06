5ee3986905f01869bec0eaa97700d64029645581
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-03-31 16:09:10 +0300

    Виконання восьмого завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
+// console.log(form);
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const emailInput = form.elements.email.value;
+  // console.dir(emailInput);
+  const passwordInput = form.elements.password.value;
+
+  if (!emailInput || !passwordInput) {
+    alert("Всі поля повинні бути заповнені!");
+
+    return;
+  } else {
+    const formData = {
+      email: emailInput,
+      password: passwordInput,
+    };
+    console.log(formData);
+    form.reset();
+  }
+}
